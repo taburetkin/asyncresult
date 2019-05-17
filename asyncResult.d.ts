7d013afce9fbf3103b4c98aabeae0ac9429e4dfc
SalathielGenese <salathielgenese@gmail.com>
2019-05-17 04:05:38 +0100

    Type definition for AsyncResult class

---
+++ asyncResult.d.ts
@@ -0,0 +1,15 @@
+export default class AsyncResult<V, E = Error>
+{
+    static fail<E>( err: E ): AsyncResult<never, E>;
+    static success<V>( data ): AsyncResult<V, never>;
+
+    public constructor( err?: E, val?: V );
+
+    hasValue(): boolean;
+    isError(): boolean;
+    isOk(): boolean;
+
+    errOrVal(): E | V | undefined;
+    err(): E | undefined;
+    val(): V | undefined;
+}
